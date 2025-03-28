3cda864a4c8db8e90022b489b0df52084be6fda6
Frank Reichert <31617868+iccander@users.noreply.github.com>
2025-03-28 15:04:57 +0100

    Update queries.php

--- queries.php
+++ queries.php
@@ -1,17 +1,25 @@
 <?php
 function quote($quote){return '"'.$quote.'"';
-}
-function sparql($query){
+} 
+function sparqlfeld($query){
     $opts=['http'=>['method'=>'GET','header'=>["Accept: application/sparql-results+json\r\nUser-Agent: curl/7.79.1\r\n"],],];
     $response=file_get_contents('https://query.wikidata.org/sparql?query='.urlencode($query),false,stream_context_create($opts));
-    return json_decode($response,true)['results']['bindings'][0];
+    return json_decode($response,true)['results']['bindings'];
 }
-function lobid($GND){
-	$ch=curl_init("https://lobid.org/gnd/".$GND.".json");
+function sparql($query){
+    return sparqlfeld($query)[0];
+}
+function jsonstring($URL){
+	$ch=curl_init($URL);
+	$useragent = 'User-Agent: curl/7.79.1';
+	curl_setopt($ch,CURLOPT_USERAGENT,$useragent);
 	curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
 	$response=curl_exec($ch);
 	curl_close($ch);
-	return json_decode($response,true);
+	return $response;
+}
+function lobid($GND){
+	return json_decode(jsonstring("https://lobid.org/gnd/".$GND.".json"),true);
 }
 function sparqlGND($gnd,$viaf){
 	if (!empty($viaf)) {$query ='{?u wdt:P227 "'.$gnd.'"} UNION {?u wdt:P214 '.$viaf.'}} GROUP BY ?u LIMIT 1';}
@@ -19,14 +27,30 @@ function sparqlGND($gnd,$viaf){
 	return sparql('SELECT DISTINCT (STRAFTER(STR(?u),"y/") AS ?q) WHERE {'.$query)['q']['value'];
 }
 function lookup($GND){
-	$Q='';
-	foreach(lobid($GND)["sameAs"] as $ids ){
-		if (($pos=strpos($ids["id"],'wikidata.org')) !== false) {
-			$Q=substr($ids["id"],$pos+20);
-			break;}
-		if (($pos=strpos($ids["id"],'viaf.org')) !== false) $viaf=quote(substr($ids["id"],$pos+14));
-	}
+	$Q='';		// 1.) in Lobid-Datensatz 
+	$result=lobid($GND);
+	if (is_array($result["sameAs"]) && $result["sameAs"]) {
+		foreach($result["sameAs"] as $ids ){
+			if (($pos=strpos($ids["id"],'wikidata.org')) !== false) {
+				$Q=substr($ids["id"],$pos+20);
+				break;}
+			if (($pos=strpos($ids["id"],'viaf.org')) !== false) $viaf=quote(substr($ids["id"],$pos+14));
+		}
+	}             // 2.) alternativ in Wikidata 
 	if ($Q=='') $Q=sparqlGND($GND, $viaf);
+	if (($Q=='') or empty($Q)) {	// 3.) alternativ in Frankreich, USA 
+		if (is_array($result["closeMatch"]) && $result["closeMatch"]) {
+			foreach($result["closeMatch"] as $ids ){
+				$auskunft='';
+				if (($pos=strpos($ids["id"],'data.bnf.fr/ark')) !== false) $ausk=jsonstring($ids["id"].".rdfjsonld");
+				if (($pos=strpos($ids["id"],'id.loc.gov/auth')) !== false) $ausk=jsonstring($ids["id"].".json");
+				if (preg_match('/wikidata\.org\/entity\/(Q\d+)/', $ausk, $treffer)) {
+					$Q=$treffer[1];
+					break;
+				}
+			}
+		}
+	}
 	return $Q;
 }
 ?>
